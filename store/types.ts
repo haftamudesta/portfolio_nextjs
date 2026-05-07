fbc8b8ff16770fb18f54e1ab83287aeb995d740b
Haftamu Desta <haftamudesta>
2026-05-07 11:53:37 -0700

    Add types

---
+++ store/types.ts
@@ -0,0 +1,17 @@
+import { StateCreator } from 'zustand';
+
+import { UISlice } from './slices/uiSlice';
+import { PersistOptions, devtools } from 'zustand/middleware';
+
+export type StoreState = UISlice;
+
+export type SliceCreator<T> = StateCreator<
+  StoreState,
+  [['zustand/persist', unknown], ['zustand/devtools', never]],
+  [],
+  T
+>;
+
+export type PersistConfig = PersistOptions<StoreState>;
+
+export type StoreType = StoreState;
